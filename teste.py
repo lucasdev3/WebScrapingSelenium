e1a1813ceafdc3098be86d0c1e5bdc554d814108
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2021-11-23 21:35:13 -0300

    update
    consertando sintaxe que estava em depreciação (find_elements_xpath) por find_elements(By.XPATH, 'xpath')

--- teste.py
+++ teste.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import csv
 from time import sleep
-
 
 navegador = webdriver.Chrome()
 
@@ -10,14 +10,27 @@ navegador.get('https://finance.yahoo.com/quote/BTC-EUR/history?p=BTC-EUR%27')
 listaDataAbertura = []
 listaFechamentoDia = []
 
-for i in range (1, 101):
-    retornoDataAbertura = navegador.find_element_by_xpath(f'//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table/tbody/tr[{i}]/td[1]').text
-    retornoFechamentoDia =  navegador.find_element_by_xpath(f'//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table/tbody/tr[{i}]/td[5]').text
+
+for i in range(1, 11):
+
+    """
+    retornoDataAbertura = navegador.find_element_by_xpath(
+        f'//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table/tbody/tr[{i}]/td[1]').text"""
+
+    retornoDataAbertura = navegador.find_element(By.XPATH,
+                                                 '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div['
+                                                 '2]/table/tbody/tr[1]/td[1]').text
+
+    """
+    retornoFechamentoDia = navegador.find_element_by_xpath(
+        f'//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table/tbody/tr[{i}]/td[5]').text"""
+
+    retornoFechamentoDia = navegador.find_element(By.XPATH,
+                                                  '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div['
+                                                  '2]/table/tbody/tr[1]/td[5]').text
+
     listaDataAbertura.append(retornoDataAbertura)
     listaFechamentoDia.append(retornoFechamentoDia)
-
-
-
 
 print(listaDataAbertura)
 print(listaFechamentoDia)
@@ -28,6 +41,7 @@ with open("names.csv", "w") as arquivo_csv:
     escrever.writeheader()
 
     for i in range(0, len(listaDataAbertura)):
-        escrever.writerow({"Data de Abertura": f"{listaDataAbertura[i]}", "Preço de Fechamento": f"{listaFechamentoDia[i]}"})
+        escrever.writerow(
+            {"Data de Abertura": f"{listaDataAbertura[i]}", "Preço de Fechamento": f"{listaFechamentoDia[i]}"})
 
     arquivo_csv.close()
