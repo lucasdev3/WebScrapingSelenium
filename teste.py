e4fbc4e0184ba48952966f61ba2f3464dc0e7f8f
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2021-11-25 00:04:48 -0300

    atualização

--- teste.py
+++ teste.py
@@ -8,40 +8,54 @@ navegador = webdriver.Chrome()
 navegador.get('https://finance.yahoo.com/quote/BTC-EUR/history?p=BTC-EUR%27')
 
 listaDataAbertura = []
+listaPrecoAbertura = []
+listaPrecoAlta = []
+listaPrecoBaixa = []
 listaFechamentoDia = []
 
-
-for i in range(1, 11):
-
-    """
-    retornoDataAbertura = navegador.find_element_by_xpath(
-        f'//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table/tbody/tr[{i}]/td[1]').text"""
-
+for i in range(1, 101):
     retornoDataAbertura = navegador.find_element(By.XPATH,
                                                  '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div['
-                                                 '2]/table/tbody/tr[1]/td[1]').text
+                                                 f'2]/table/tbody/tr[{i}]/td[1]').text
 
-    """
-    retornoFechamentoDia = navegador.find_element_by_xpath(
-        f'//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table/tbody/tr[{i}]/td[5]').text"""
+    retornoPrecoAbertura = navegador.find_element(By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div['
+                                                            f'2]/table/tbody/tr[{i}]/td[2]').text
+
+    retornoPrecoAlta = navegador.find_element(By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div['
+                                                        f'2]/table/tbody/tr[{i}]/td[3]').text
+
+    retornoPrecoBaixa = navegador.find_element(By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div['
+                                                         f'2]/table/tbody/tr[{i}]/td[4]').text
 
     retornoFechamentoDia = navegador.find_element(By.XPATH,
                                                   '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div['
-                                                  '2]/table/tbody/tr[1]/td[5]').text
+                                                  f'2]/table/tbody/tr[{i}]/td[5]').text
 
     listaDataAbertura.append(retornoDataAbertura)
+    listaPrecoAbertura.append(retornoPrecoAbertura)
+    listaPrecoAlta.append(retornoPrecoAlta)
+    listaPrecoBaixa.append(retornoPrecoBaixa)
     listaFechamentoDia.append(retornoFechamentoDia)
 
+
 print(listaDataAbertura)
+print(listaPrecoAbertura)
+print(listaPrecoAlta)
+print(listaPrecoBaixa)
 print(listaFechamentoDia)
 
 with open("names.csv", "w") as arquivo_csv:
-    colunas = ["Data de Abertura", "Preço de Fechamento"]
+    colunas = ["Data de Abertura", "preço de abertura", "preço em alta", "preço em baixa", "preço de fechamento"]
     escrever = csv.DictWriter(arquivo_csv, fieldnames=colunas, delimiter=";", lineterminator="\n")
     escrever.writeheader()
 
     for i in range(0, len(listaDataAbertura)):
         escrever.writerow(
-            {"Data de Abertura": f"{listaDataAbertura[i]}", "Preço de Fechamento": f"{listaFechamentoDia[i]}"})
+            {"Data de Abertura": f"{listaDataAbertura[i]}",
+             "preço de abertura": f"{listaPrecoAbertura[i]}",
+             "preço em alta": f"{listaPrecoAlta[i]}",
+             "preço em baixa": f"{listaPrecoBaixa[i]}",
+             "preço de fechamento": f"{listaFechamentoDia[i]}"
+             })
 
     arquivo_csv.close()
--- teste2.py
+++ teste2.py
@@ -1,7 +1,2 @@
-import csv
-
-with open("names.csv", "w") as arquivo_csv:
-    colunas = ["nome", "profissao"]
-    escrever = csv.DictWriter(arquivo_csv, fieldnames=colunas, delimiter=";", lineterminator="\n")
-    escrever.writeheader()
-    escrever.writerow({"nome": "Renato", "profissao": "programador"})
+texto = "51295.57"
+print(float(texto))
